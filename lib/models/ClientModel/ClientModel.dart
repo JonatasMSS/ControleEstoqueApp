27aff2493b4c8e68aa6971b5840fc1a972ced831
JonatasMSS <jonatas.miguelss@gmail.com>
2023-01-09 09:48:03 -0300

    models criado

---
+++ lib/models/ClientModel/ClientModel.dart
@@ -0,0 +1,72 @@
+import 'dart:convert';
+
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class ClientModel {
+  int id;
+  String name;
+  String number;
+  String date;
+  ClientModel({
+    required this.id,
+    required this.name,
+    required this.number,
+    required this.date,
+  });
+
+  ClientModel copyWith({
+    int? id,
+    String? name,
+    String? number,
+    String? date,
+  }) {
+    return ClientModel(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      number: number ?? this.number,
+      date: date ?? this.date,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'number': number,
+      'date': date,
+    };
+  }
+
+  factory ClientModel.fromMap(Map<String, dynamic> map) {
+    return ClientModel(
+      id: map['id'] as int,
+      name: map['name'] as String,
+      number: map['number'] as String,
+      date: map['date'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory ClientModel.fromJson(String source) =>
+      ClientModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'ClientModel(id: $id, name: $name, number: $number, date: $date)';
+  }
+
+  @override
+  bool operator ==(covariant ClientModel other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id &&
+        other.name == name &&
+        other.number == number &&
+        other.date == date;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^ name.hashCode ^ number.hashCode ^ date.hashCode;
+  }
+}
